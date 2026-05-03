580e40a4eee29c34e39ae0e2b1ef6710aa6d87bb
Menna003 <mennaa101200@gmail.com>
2026-05-02 18:44:04 -0700

    enhance mvp structure

--- SportsApp/Utils/Constants.swift
+++ SportsApp/Utils/Constants.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct API {
+    static let key = "8885a38d10bd401e9db38d09b946c11a4d059014e8028004246c1b052e43b1e9"
+}
--- SportsApp/Utils/UIColor+Extension.swift
+++ SportsApp/Utils/UIColor+Extension.swift
@@ -5,4 +5,18 @@
 //  Created by Manona on 02/05/2026.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let r = CGFloat((int >> 16) & 0xFF) / 255
+        let g = CGFloat((int >> 8) & 0xFF) / 255
+        let b = CGFloat(int & 0xFF) / 255
+        
+        self.init(red: r, green: g, blue: b, alpha: 1)
+    }
+}
