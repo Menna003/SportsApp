62bc6214009966c78ebe9665459b1ce99d0ef5a1
Menna003 <mennaa101200@gmail.com>
2026-05-02 18:43:09 -0700

    Create LeagueCell UI component

--- SportsApp/Features/Leagues/View/LeagueCell.swift
+++ SportsApp/Features/Leagues/View/LeagueCell.swift
@@ -6,18 +6,57 @@
 //
 
 import UIKit
+import SDWebImage
 
 class LeagueCell: UITableViewCell {
 
+    @IBOutlet weak var leagueImage: UIImageView!
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var leagueNameLabel: UILabel!
+    
+    var isFavorite = false
+    var onFavTapped: (() -> Void)?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        contentView.layer.borderWidth = 1
+        contentView.layer.borderColor = UIColor.black.cgColor
+        contentView.layer.cornerRadius = 32
+        contentView.layer.masksToBounds = true
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5))
+        
+        leagueImage.layer.cornerRadius = leagueImage.frame.height / 2
+        leagueImage.clipsToBounds = true
+    }
+    
+    func updateFavUI() {
+        let imageName = isFavorite ? "fav-icon" : "fav-icon-outline"
+        favButton.setImage(UIImage(named: imageName), for: .normal)
+    }
+    
+    @IBAction func favBtn(_ sender: Any) {
+        onFavTapped?()
+    }
+    
+    func configure(with league: League, isFavorite: Bool) {
+        
+        leagueNameLabel.text = league.leagueName ?? "No Name"
+        
+        self.isFavorite = isFavorite
+        updateFavUI()
+        
+        if let urlString = league.leagueLogo,
+           let url = URL(string: urlString) {
+            
+            leagueImage.sd_setImage(with: url, placeholderImage: UIImage(named: "placeholder-league"))
+        } else {
+            leagueImage.image = UIImage(named: "placeholder-league")
+        }
+    }
 }
