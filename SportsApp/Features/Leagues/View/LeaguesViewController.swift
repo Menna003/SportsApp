47c73ef010e8d941a73b1d9b73f17ac808738828
Menna003 <mennaa101200@gmail.com>
2026-05-02 18:43:22 -0700

    Handle LeaguesViewController and bind MVP components

--- SportsApp/Features/Leagues/View/LeaguesViewController.swift
+++ SportsApp/Features/Leagues/View/LeaguesViewController.swift
@@ -7,23 +7,89 @@
 
 import UIKit
 
-class LeaguesViewController: UIViewController {
-
+class LeaguesViewController: UIViewController, LeaguesViewProtocol, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var sportName: String?
+    let presenter = LeaguesPresenter()
+    var leagues: [League] = []
+    
+    @IBOutlet weak var leaguesTableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        leaguesTableView.register(UINib(nibName: "LeagueCell", bundle: nil), forCellReuseIdentifier: "LeagueCell")
+        leaguesTableView.delegate = self
+        leaguesTableView.dataSource = self
+        
+        searchBar.delegate = self
+        
+        let textField = searchBar.searchTextField
+        textField.backgroundColor = UIColor(red: 229/255, green: 229/255, blue: 234/255, alpha: 1)
+        textField.layer.cornerRadius = 10
+        textField.clipsToBounds = true
+        textField.layer.borderWidth = 0
+        searchBar.backgroundImage = UIImage()
+        
+        presenter.view = self
+        
+        if let sport = sportName {
+            presenter.getLeagues(for: sport)
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    func showLeagues(_ leagues: [League]) {
+        self.leagues = leagues
+        
+        DispatchQueue.main.async {
+            self.leaguesTableView.reloadData()
+        }
+    }
+    
+    func showError() {
+        print("Failed to load leagues")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return leagues.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LeagueCell", for: indexPath) as! LeagueCell
+        
+        let league = leagues[indexPath.row]
+        
+        let isFav = presenter.isFavorite(id: league.leagueKey)
+        
+        cell.configure(with: league, isFavorite: isFav)
+        
+        cell.onFavTapped = { [weak self] in
+            guard let self = self else { return }
+            
+            self.presenter.toggleFavorite(id: league.leagueKey)
+            self.leaguesTableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        presenter.search(text: searchText)
+    }
 }
