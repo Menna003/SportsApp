8fb01cab22a5627bc1b4dcce9333fd776fd0c5d0
Menna003 <mennaa101200@gmail.com>
2026-05-08 11:59:43 -0700

    Create HeaderCell XIB with dynamic constraints

---
+++ SportsApp/Features/LeaguesDetails/View/HeaderCell.swift
@@ -0,0 +1,23 @@
+//
+//  HeaderCell.swift
+//  SportsApp
+//
+//  Created by Manona on 08/05/2026.
+//
+
+import UIKit
+
+class HeaderCell: UICollectionViewCell {
+
+    @IBOutlet weak var leaguesTitle: UILabel!
+    @IBOutlet weak var leaguesImage: UIImageView!
+    @IBOutlet weak var countryLogo: UIImageView!
+    @IBOutlet weak var seasonLabel: UILabel!
+    @IBOutlet weak var countTeam: UILabel!
+    @IBOutlet weak var countryTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
