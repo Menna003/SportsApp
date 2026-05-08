2d36ec70197a290dd7747b4ae9025d79600b442a
Menna003 <mennaa101200@gmail.com>
2026-05-08 13:24:56 -0700

    Create UpcomigCell XIB

---
+++ SportsApp/Features/LeaguesDetails/View/UpcomingCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingCell.swift
+//  SportsApp
+//
+//  Created by Manona on 08/05/2026.
+//
+
+import UIKit
+
+class UpcomingCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
