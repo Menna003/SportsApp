7dd137d2c6f2e4e009e5774b9bdc53856afd89c8
Menna003 <mennaa101200@gmail.com>
2026-05-08 15:43:42 -0700

    add teams cell with dynamic constraints

---
+++ SportsApp/Features/LeaguesDetails/View/TeamsCell.swift
@@ -0,0 +1,19 @@
+//
+//  TeamsCell.swift
+//  SportsApp
+//
+//  Created by Manona on 08/05/2026.
+//
+
+import UIKit
+
+class TeamsCell: UICollectionViewCell {
+
+    @IBOutlet weak var teamImage: UIImageView!
+    @IBOutlet weak var teamLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
