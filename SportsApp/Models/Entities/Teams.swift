3b1215fa4b6e3f7e9d34b869ab2f7ed978a3bd83
Menna003 <mennaa101200@gmail.com>
2026-05-09 15:27:27 -0700

    Add Player model and extend Team with players

--- SportsApp/Models/Entities/Teams.swift
+++ SportsApp/Models/Entities/Teams.swift
@@ -6,19 +6,48 @@
 //
 
 
+import Foundation
+
 struct TeamsResponse: Decodable {
     let success: Int?
     let result: [Team]?
 }
 
 struct Team: Decodable {
-    let teamKey: Int?
+    let teamKey:  Int?
     let teamName: String?
     let teamLogo: String?
-    
+    let players:  [Player]?
+
     enum CodingKeys: String, CodingKey {
-        case teamKey = "team_key"
+        case teamKey  = "team_key"
         case teamName = "team_name"
         case teamLogo = "team_logo"
+        case players
+    }
+}
+
+struct TeamDetailsResponse: Decodable {
+    let success: Int?
+    let result: [Team]?
+}
+
+struct Player: Decodable {
+    let playerKey:     Int?
+    let playerName:    String?
+    let playerNumber:  String?
+    let playerImage:   String?
+    let playerType:    String?
+    let playerAge:     String?
+    let playerCountry: String?
+
+    enum CodingKeys: String, CodingKey {
+        case playerKey     = "player_key"
+        case playerName    = "player_name"
+        case playerNumber  = "player_number"
+        case playerImage   = "player_image"
+        case playerType    = "player_type"
+        case playerAge     = "player_age"
+        case playerCountry = "player_country"
     }
 }
