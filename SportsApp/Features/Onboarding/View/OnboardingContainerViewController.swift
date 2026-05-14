4d7f1034cadb69e26135ee76361de4848976eacc
Menna003 <mennaa101200@gmail.com>
2026-05-14 12:33:53 -0700

    Create onboarding structure and screens

---
+++ SportsApp/Features/Onboarding/View/OnboardingContainerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingContainerViewController.swift
+//  SportsApp
+//
+//  Created by Manona on 14/05/2026.
+//
+
+import UIKit
+
+class OnboardingContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
