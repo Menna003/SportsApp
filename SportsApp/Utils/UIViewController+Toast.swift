698af2144bec6274366afb1952453597a9c71eef
Menna003 <mennaa101200@gmail.com>
2026-05-16 11:59:21 -0700

    Update toast style

--- SportsApp/Utils/UIViewController+Toast.swift
+++ SportsApp/Utils/UIViewController+Toast.swift
@@ -11,53 +11,51 @@ extension UIViewController {
 
     func showToast(message: String) {
 
-        let container = UIView()
-        container.backgroundColor = UIColor.black.withAlphaComponent(0.85)
-        container.layer.cornerRadius = 16
-        container.clipsToBounds = true
+        let toastLabel = UILabel()
 
-        let label = UILabel()
-        label.text = message
-        label.textColor = .white
-        label.font = .systemFont(ofSize: 15, weight: .medium)
-        label.numberOfLines = 0
-        label.textAlignment = .center
+        toastLabel.backgroundColor = .white
+        toastLabel.textColor = .black
+        toastLabel.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        toastLabel.textAlignment = .center
+        toastLabel.text = message
+        toastLabel.alpha = 0
 
-        container.addSubview(label)
-        view.addSubview(container)
+        toastLabel.layer.cornerRadius = 14
+        toastLabel.clipsToBounds = true
 
-        container.translatesAutoresizingMaskIntoConstraints = false
-        label.translatesAutoresizingMaskIntoConstraints = false
+        toastLabel.layer.borderWidth = 1
+        toastLabel.layer.borderColor = UIColor.black.withAlphaComponent(0.15).cgColor
 
-        NSLayoutConstraint.activate([
+        toastLabel.numberOfLines = 0
 
-            container.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            container.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
-            container.widthAnchor.constraint(lessThanOrEqualToConstant: 280),
+        let maxWidth = self.view.frame.width - 40
 
-            label.topAnchor.constraint(equalTo: container.topAnchor, constant: 12),
-            label.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -12),
-            label.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 12),
-            label.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -12),
-        ])
+        let expectedSize = toastLabel.sizeThatFits(
+            CGSize(width: maxWidth, height: .greatestFiniteMagnitude)
+        )
 
-        container.alpha = 0
-        container.transform = CGAffineTransform(translationX: 0, y: 20)
+        toastLabel.frame = CGRect(
+            x: 20,
+            y: self.view.frame.height - 120,
+            width: maxWidth,
+            height: expectedSize.height + 20
+        )
+
+        self.view.addSubview(toastLabel)
 
         UIView.animate(withDuration: 0.3) {
-            container.alpha = 1
-            container.transform = .identity
-        }
-
-        UIView.animate(
-            withDuration: 0.3,
-            delay: 2,
-            options: .curveEaseOut
-        ) {
-            container.alpha = 0
-            container.transform = CGAffineTransform(translationX: 0, y: 20)
+            toastLabel.alpha = 1
         } completion: { _ in
-            container.removeFromSuperview()
+
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 2,
+                options: .curveEaseOut
+            ) {
+                toastLabel.alpha = 0
+            } completion: { _ in
+                toastLabel.removeFromSuperview()
+            }
         }
     }
 }
