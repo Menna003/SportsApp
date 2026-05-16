2fed592a1d0df49178efe9ee1fd8ebe6f9fd7a8c
Menna003 <mennaa101200@gmail.com>
2026-05-16 04:30:28 -0700

    Add onboarding content xib design

--- SportsApp/Features/Onboarding/View/OnboardingContentViewController.swift
+++ SportsApp/Features/Onboarding/View/OnboardingContentViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class OnboardingContentViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    var imageName: String?
+    var titleText: String?
+    var descriptionText: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        backgroundImageView.image = UIImage(named: imageName ?? "")
+        titleLabel.text = titleText
+        descriptionLabel.text = descriptionText
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
